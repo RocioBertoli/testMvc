aebcec1e749039f4116622cf007692a3d284eeda
rociobertoli <rociobertoli@gmail.com>
2016-08-12 15:44:14 -0300

    project initialization

---
+++ MvcMovie/Dal/MoviesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MvcMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcMovie.Dal
+{
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
